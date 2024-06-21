48f821b3d38227f2d0b9669195dcf3df8b99b9ef
Roman Kalyakin <roman@kalyakin.com>
2024-06-21 15:52:03 +0200

    prepare slack notification, unknown user auth test, vscode support

--- cypress/e2e/1-basic/auth.cy.ts
+++ cypress/e2e/1-basic/auth.cy.ts
@@ -10,4 +10,15 @@ describe('Authentication', () => {
     cy.get('.HomePage__hugeHeading').contains('Media Monitoring of the Past')
     cy.logout()
   })
+
+  it('an unknown user cannot log in', () => {
+    cy.login(undefined, true, 'unknown.user@impresso-project.ch', 'nothing')
+
+    // there is an error
+    cy.get('#UserLoginPage .alert-danger').as('error')
+    cy.get('@error').should('contain.text', 'Login Failed!')
+
+    // there is still a login link (not logged in)
+    cy.get('.TheHeader__userArea .nav-link .small-caps').first().should('have.text', 'login')
+  })
 })
